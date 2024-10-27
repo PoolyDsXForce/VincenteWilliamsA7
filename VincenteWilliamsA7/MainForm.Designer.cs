19560f051b9e2376cc4a90b8c03dece1433b88fe
PoolyDsXForce <124118232+PoolyDsXForce@users.noreply.github.com>
2024-10-27 16:20:44 -0400

    Add files via upload

---
+++ VincenteWilliamsA7/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace VincenteWilliamsA7
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Deigner generated code
+
+        /// <summary>
+        /// Initializes the component.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CarsListBox = new ListBox();
+            SortByMakeBtn = new Button();
+            SortByMakeAndPriceBtn = new Button();
+            ChooseFileBtn = new Button();
+            SuspendLayout();
+            // 
+            // CarsListBox
+            // 
+            CarsListBox.FormattingEnabled = true;
+            CarsListBox.Location = new Point(213, 24);
+            CarsListBox.Name = "CarsListBox";
+            CarsListBox.Size = new Size(561, 404);
+            CarsListBox.TabIndex = 0;
+            // 
+            // SortByMakeBtn
+            // 
+            SortByMakeBtn.Location = new Point(13, 84);
+            SortByMakeBtn.Name = "SortByMakeBtn";
+            SortByMakeBtn.Size = new Size(116, 29);
+            SortByMakeBtn.TabIndex = 1;
+            SortByMakeBtn.Text = "Sort By Make";
+            SortByMakeBtn.UseVisualStyleBackColor = true;
+            SortByMakeBtn.Click += SortByMakeBtn_Click;
+            // 
+            // SortByMakeAndPriceBtn
+            // 
+            SortByMakeAndPriceBtn.Location = new Point(12, 128);
+            SortByMakeAndPriceBtn.Name = "SortByMakeAndPriceBtn";
+            SortByMakeAndPriceBtn.Size = new Size(175, 29);
+            SortByMakeAndPriceBtn.TabIndex = 2;
+            SortByMakeAndPriceBtn.Text = "Sort By Make and Price";
+            SortByMakeAndPriceBtn.UseVisualStyleBackColor = true;
+            SortByMakeAndPriceBtn.Click += SortByMakeAndPriceBtn_Click;
+            // 
+            // ChooseFileBtn
+            // 
+            ChooseFileBtn.Location = new Point(12, 34);
+            ChooseFileBtn.Name = "ChooseFileBtn";
+            ChooseFileBtn.Size = new Size(94, 29);
+            ChooseFileBtn.TabIndex = 3;
+            ChooseFileBtn.Text = "Choose File";
+            ChooseFileBtn.UseVisualStyleBackColor = true;
+            ChooseFileBtn.Click += ChooseFileBtn_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ChooseFileBtn);
+            Controls.Add(SortByMakeAndPriceBtn);
+            Controls.Add(SortByMakeBtn);
+            Controls.Add(CarsListBox);
+            Name = "MainForm";
+            Text = "Vincente Williams Assignment 7";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox CarsListBox;
+        private Button SortByMakeBtn;
+        private Button SortByMakeAndPriceBtn;
+        private Button ChooseFileBtn;
+    }
+}
---
+++ VincenteWilliamsA7/Model/Car.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VincenteWilliamsA7.Model
+{
+    /// <summary>
+    /// This is the car class that stores the car data from Json. 
+    /// </summary>
+    /// <seealso cref="System.IComparable&lt;VincenteWilliamsA7.Model.Car&gt;" />
+    public class Car : IComparable<Car>
+    {
+        /// <summary>
+        /// Gets or sets the make.
+        /// </summary>
+        /// <value>
+        /// The make.
+        /// </value>
+        [JsonPropertyName("make")]
+        public string? Make { get; set; }
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        /// <value>
+        /// The model.
+        /// </value>
+        [JsonPropertyName("model")]
+        public string? Model { get; set; }
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+        /// <summary>
+        /// Gets or sets the mileage.
+        /// </summary>
+        /// <value>
+        /// The mileage.
+        /// </value>
+        [JsonPropertyName("mileage")]
+        public int Mileage { get; set; }
+        /// <summary>
+        /// Gets or sets the color.
+        /// </summary>
+        /// <value>
+        /// The color.
+        /// </value>
+        [JsonPropertyName("color")]
+        public string? Color { get; set; }
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <param name="other">An object to compare with this instance.</param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being compared. The return value has these meanings:
+        /// <list type="table"><listheader><term> Value</term><description> Meaning</description></listheader><item><term> Less than zero</term><description> This instance precedes <paramref name="other" /> in the sort order.</description></item><item><term> Zero</term><description> This instance occurs in the same position in the sort order as <paramref name="other" />.</description></item><item><term> Greater than zero</term><description> This instance follows <paramref name="other" /> in the sort order.</description></item></list>
+        /// </returns>
+        public int CompareTo(Car other)
+        {
+            return this.Make.CompareTo(other.Make);
+        }
+    }
+}
